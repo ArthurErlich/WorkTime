ebc9e936808f99a38e81465f861e8d6a6114ddeb
ArthurErlich <a.erlich@live.de>
2024-09-11 21:06:43 +0200

    removed comments

--- src/Command/Convert.php
+++ src/Command/Convert.php
@@ -16,9 +16,6 @@ class Convert extends Command
 {
     public function __construct()
     {
-        // best practices recommend to call the parent constructor first and
-        // then set your own properties. That wouldn't work in this case
-        // because configure() needs the properties set in this constructor
         parent::__construct();
     }
 
@@ -28,26 +25,17 @@ class Convert extends Command
         $format = $input->getOption('format');
 
         $io->title("Work Time converter");
-        if($format != null){
-            $format = strtolower($format);
-            $io->note('Selected Format: '.$format);
+        if($format == null){
+           return self::INVALID;
         }
-        // ... put here the code to create the user
+        //check for format. Simple for now
+        $format = strtolower($format);
+        if($format !== "awork"){
+            return self::INVALID;
+        }
+        $io->note('Selected Format: '.$format);
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
-
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
     }
 
     protected function configure(): void
